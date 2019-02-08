6ea65ae989501395a1670847665fa469c2c625a1
Krishna Teja <s533708@nwmissouri.edu>
2019-02-08 15:41:28 -0600

    Added CarTestController in tests

---
+++ tests/UnitTests/Web/Controllers/CarControllerTests.cs
@@ -0,0 +1,51 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Web.Controllers;
+using Xunit;
+
+using Web;
+using Microsoft.AspNetCore.Mvc.Testing;
+using System.Net;
+using System.Net.Http;
+
+
+// Important: Make sure namespace matches folder structure
+namespace Tests.UnitTests.Web.Controllers
+{
+    public class HomeControllerTests
+    {
+
+    [Fact]
+        public void InitialsTest()
+        {
+            var expected = "P.T.R.T.";
+            var name="Peddi Taraka Ravi Teja";
+            var actual = Initials(name);
+            Assert.Equal(expected, actual);
+        }
+
+        [Theory]
+        [InlineData("Pulihora Raja","P.R.")]
+        [InlineData("Shivaji Rao Gaekwad","S.R.G.")]
+        [InlineData("Kolar Gold Fields","K.G.F.")]
+        public void IsOdd_WorksWithManyInputs(string name, string exp)
+        {
+            Assert.Equal(Initials(name),exp);
+        }
+
+        public string Initials(string name)
+        {
+            string[] temp;
+            string res="";
+            temp=name.Split(" ");
+            foreach(String item in temp){
+                res+=item.Substring(0,1)+".";
+            }
+            return res;
+        }
+
+    }
+}
