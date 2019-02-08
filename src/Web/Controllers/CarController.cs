10eb8192afc546405254d41ccfb4c1003efaa3e2
Krishna Teja <s533708@nwmissouri.edu>
2019-02-08 13:03:22 -0600

    Updated with Text/HTML/JSON/View

--- src/Web/Controllers/CarController.cs
+++ src/Web/Controllers/CarController.cs
@@ -15,14 +15,29 @@ namespace Web.Controllers
             return View();
         }
 
-        public IActionResult Lamborghini(){
-            return View();
+        public IActionResult Lamborghini()
+        {
+                
+            return Json(new { success = true, brand = "Lamborghini", model = "Gallardo", type = "JSON"});
+        }
+
+        public IActionResult AlfaRomeo()
+        {
+
+            return Content("Alfa Romeo is another sport brand. Returned in text.");
+        }
+
+        public IActionResult Porsche()
+        {
+
+            return Content("<html><head><title>Porsche Carrera</title></head><body>Porsche, Returned in HTML</body></html>","text/html");
         }
 
         public IActionResult Ferrari()
         {
             return View();
         }
+        
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
